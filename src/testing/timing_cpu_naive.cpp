cc3530d4d4cd25b28fb6cc9932b1b11450995b68
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-05-22 21:41:46 -0400

    Add in CUDA_DIR

--- src/testing/timing_cpu_naive.cpp
+++ src/testing/timing_cpu_naive.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <algorithm>
+#include <cstdlib>
 
 #include "CPU_PairCounts.h"
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
 	cout << "A simple test of the pair counting code" << endl;
-	RHist rr(20, 0.0, 0.1);
+
+	int Npart = atoi(argv[1]);
+	int Nbins = atoi(argv[2]);
+	cout << "Using N particles = " << Npart << endl;
+	cout << "Using N histogram bins = " << Nbins << endl;
+
+	RHist rr(Nbins, 0.0, 1.0/static_cast<double>(Nbins));
 
 	CPUParticles p1, p2;
 	{
 		CPUclock t1;
-		p1.mkRandom(10, 111);
-		p2.mkRandom(20, 129);
+		p1.mkRandom(Npart, 111);
+		p2.mkRandom(Npart, 129);
 		double dt = t1.elapsed();
 		cout << "Time to generate random particles (ms) ::" << dt << endl;
 	}
@@ -24,13 +31,5 @@ int main() {
 		double dt = t1.elapsed();
 		cout << "Time to count the pairs (ms) :: " << dt << endl;
 	}
-
-	unsigned long long sum=0ll;
-	cout << "The histogram is :";
-	for_each(rr.hist.begin(), rr.hist.end(), [&sum](unsigned long long ii){
-		cout << ii << " ";
-		sum += ii;});
-	cout << endl << "The sum of the histogram is :" << sum << endl;
-
 
 }
